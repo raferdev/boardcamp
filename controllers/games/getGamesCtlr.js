2dae527e809e037cfd652ff656c1cba2e5cd1978
Rafael <rafinha_fern@hotmail.com>
2022-05-30 13:53:15 -0300

    getGamesController logic's to querystring name in request, and filtering this games

--- controllers/games/getGamesCtlr.js
+++ controllers/games/getGamesCtlr.js
@@ -1,6 +1,22 @@
 import db from "../../db/db.js"
 
 async function getGamesCtlr(req,res) {
+    let name = req.query.name + "%";
+    name = name[0].toUpperCase() + name.slice(1);
+
+    if(name) {
+        try {
+            const queryGames = await db.query(`
+            SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON "categoryId" = categories.id WHERE games.name LIKE $1 ;
+            `,[name]);
+
+            const games = queryGames.rows;
+            return res.send(games)
+        } catch(e) {
+            console.log(e)
+           return res.sendStatus(500)
+        }
+    }
 
     try {
         const queryGames = await db.query(`
