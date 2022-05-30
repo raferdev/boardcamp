9f17d6443b14ad89be725ae6ae543f61eed379c2
Rafael <rafinha_fern@hotmail.com>
2022-05-30 14:26:26 -0300

    getCustomers with queryString filtering

--- controllers/customers/getCustomersCtlr.js
+++ controllers/customers/getCustomersCtlr.js
@@ -1,6 +1,22 @@
 import db from "../../db/db.js"
 
 async function getCustomersCtlr(req,res) {
+  let cpf = req.query.cpf;
+    
+    if(cpf) {
+        cpf += "%";
+        try {
+            const queryCustomers = await db.query(`
+            SELECT * FROM customers WHERE cpf LIKE $1 ;
+            `,[cpf]);
+
+            const customers = queryCustomers.rows;
+            return res.send(customers)
+        } catch(e) {
+            console.log(e)
+           return res.sendStatus(500)
+        }
+    }
     try {
 
       const queryCustomers = await db.query(
