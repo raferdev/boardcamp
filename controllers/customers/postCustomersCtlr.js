f24a5ca35104b5ff9f970539c1ff7b6324539a21
Rafael <rafinha_fern@hotmail.com>
2022-05-30 03:02:44 -0300

    postCustomers controller insert into custumers

--- controllers/customers/postCustomersCtlr.js
+++ controllers/customers/postCustomersCtlr.js
@@ -1,6 +1,21 @@
-function postCustomersCtlr(req,res) {
-    
+import db from "../../db/db.js";
+
+async function postCustomersCtlr(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+
+  try {
+    await db.query(
+      `
+    INSERT INTO customers (name,phone,cpf,birthday) VALUES ($1,$2,$3,$4);
+    `,
+      [name, phone, cpf, birthday]
+    );
+
     res.sendStatus(201);
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(500);
+  }
 }
 
 export default postCustomersCtlr;
