ef42f8588e0271b40505aadd640885d4abc55494
Rafael <rafinha_fern@hotmail.com>
2022-05-30 03:10:16 -0300

    getCustomers controller return all data

--- controllers/customers/getCustomersCtlr.js
+++ controllers/customers/getCustomersCtlr.js
@@ -1,7 +1,20 @@
-function getCustomersCtlr(req,res) {
+import db from "../../db/db.js"
 
-    res.sendStatus(201);
+async function getCustomersCtlr(req,res) {
+    try {
 
+      const queryCustomers = await db.query(
+        `
+      SELECT * FROM customers;
+      `);
+
+      const customers = queryCustomers.rows;
+
+      res.send(customers);
+    } catch (e) {
+      console.log(e)
+      res.sendStatus(500);
+    }
 }
 
 export default getCustomersCtlr;
