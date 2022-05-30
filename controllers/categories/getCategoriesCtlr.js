e01b8dc5236812a8638a581abd0386300139690a
Rafael <rafinha_fern@hotmail.com>
2022-05-30 01:49:57 -0300

    getCategories select from categories working

--- controllers/categories/getCategoriesCtlr.js
+++ controllers/categories/getCategoriesCtlr.js
@@ -1,6 +1,20 @@
-function getCategoriesCtlr(req,res) {
+import db from "../../db/db.js";
 
-    res.sendStatus(201)
+async function getCategoriesCtlr(req,res) {
+
+    try {
+        const queryCategories = await db.query(`
+    
+        SELECT (name) FROM categories;
+    
+        `);
+        const categories = queryCategories.rows
+
+        res.send(categories);
+        } catch(e) {
+            console.log(e)
+        res.sendStatus(500);  
+        }
     }
     
 export default getCategoriesCtlr;
