34456537625def8569b0dae0b5d1dd3f382a6850
Rafael <rafinha_fern@hotmail.com>
2022-05-30 05:57:57 -0300

    getRentals controller returning all rentals with object format

--- controllers/rentals/getRentalsCtlr.js
+++ controllers/rentals/getRentalsCtlr.js
@@ -1,4 +1,34 @@
-function getRentalsCtlr(req,res) {
-    res.sendStatus(201);
+import db from "../../db/db.js";
+
+
+async function getRentalsCtlr(req,res) {
+    const rentals = [];
+   try {
+    const queryRentals = await db.query(`
+    SELECT json_build_object(
+        'id', rentals.id,
+        'customerId', rentals."customerId",
+        'gameId', rentals."gameId",
+        'rentDate', rentals."rentDate",
+        'daysRented', rentals."daysRented",
+        'returnDate', rentals."returnDate",
+        'originalPrice',rentals."originalPrice",
+        'delayFee', rentals."delayFee",
+        'customer', json_build_object('id', customers.id, 'name',customers.name),
+        'game', json_build_object('id', games.id, 'name', games.name, 'categoryId' ,games."categoryId",'categoryName', categories.name)
+    ) FROM rentals JOIN customers ON "customerId" = customers.id JOIN games ON "gameId" = games.id JOIN categories ON "categoryId" = categories.id;   
+    `);
+    
+    const rentalsKeys = queryRentals.rows;
+
+    rentalsKeys.forEach(key => {
+        rentals.push(key.json_build_object);
+    });
+
+    res.send(rentals)
+   } catch(e) {
+       console.log(e)
+    res.sendStatus(500);
+   }
 }
 export default getRentalsCtlr;
